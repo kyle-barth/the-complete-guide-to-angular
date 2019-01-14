f3a8cfef49242ae069516cc98fa19c44919d9005
kyle-barth <kylebarth@me.com>
2019-01-14 15:36:49 +0000

    Lecture 63

--- 5. Components & Databinding/src/app/server-element/server-element.component.ts
+++ 5. Components & Databinding/src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, contrnt: string};
+  @Input('srvElement') element: {type: string, name: string, contrnt: string};
 
   constructor() { }
 
