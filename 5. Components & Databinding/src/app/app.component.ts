2e38dd610c78cda4a49ac74ee5316a9f0aee9759
kyle-barth <kylebarth@me.com>
2019-01-14 15:12:59 +0000

    attempting hotfix

--- 5. Components & Databinding/src/app/app.component.ts
+++ 5. Components & Databinding/src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   serverElements = [];
   newServerName = '';
   newServerContent = '';
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
 }
